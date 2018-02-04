4927e555140045d22b547cc2dc38ad5d0e0106b3
Kimaya Desai <kimaya.desai@gmail.com>
2018-02-04 14:06:18 -0800

    Add files via upload

---
+++ UniqueString.java
@@ -0,0 +1,26 @@
+class UniqueString {
+
+static boolean anyDupChar(String str) {
+
+	if len(str) > 128 {
+		return false;
+	}
+	
+	boolean[] str_array = new boolean[128];
+
+    for (int i = 0 ; i< str.length();i++){
+    	int val = str.charAt(i);
+       	if (str_array[val])	  {
+	      	return false;
+       	}
+       	str_array [val] = true;
+       	System.out.println(str_array[val]);
+	 }
+  	return true;
+}
+
+public static void main(String args[]){
+	String str = "Kim";
+	System.out.println(anyDupChar(str));
+ }
+}
